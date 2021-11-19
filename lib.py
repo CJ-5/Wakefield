7d29180c68cf158756ade9508bc8246c10976aa1
cj-5 <2006carter.man@gmail.com>
2021-11-19 18:03:42 -0500

    Config File Implementations

---
+++ lib.py
@@ -0,0 +1,2 @@
+#  Holds the main functions that operate the backend of the game (e.g battle system)
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Base file, contains the actual output of the game
+import configparser
+
+config_obj = configparser.ConfigParser()
+config_obj.read("C:/Users/carte/Documents/GitHub/Python_Adventure_Game/config.ini")
+
 
