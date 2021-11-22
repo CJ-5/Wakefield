c1a96b2f93e6baa6f0744d9c5c0a0c0134a4aac5
Carter Jeffries <2006carter.man@gmail.com>
2021-11-22 07:31:39 -0500

    Movement Engine and Print out system

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # Base file, contains the actual output of the game
+from colorama import init
+from colorama import Fore, Back, Style
 import configparser
+import time
+import lib
+import movement_engine
+import sys
+import game_data
+
+
+init()
 
 config_obj = configparser.ConfigParser()
 config_obj.read("C:/Users/carte/Documents/GitHub/Python_Adventure_Game/config.ini")
 
+lib.print_logo()
+lib.gprint("Welcome to the game, I couldn't be bothered to name it so its called 'Adventure Game'!")
+lib.gprint("Here is some info to help you get started")
 
+lib.gprint(lib.MQ([lib.ck("- At anytime during the game if you want to see a list of all available actions simply use"),
+                   ]))
---
+++ movement_engine.py
@@ -0,0 +1,116 @@
+import game_data
+from colorama import init
+from colorama import Fore, Back, Style
+import os
+from pynput import keyboard
+from threading import Thread
+import time
+import gc
+
+init()
+
+
+# Note: Create map then use reverse operation for coordinate reading otherwise everything will be confusing
+def get_coord(map_in):
+    map_in = map_in.map_array[::-1]  # Reverse the map
+    for y in range(len(map_in)):
+        for x in range(len(map_in[y])):
+            if map_in[y][x] == "x":
+                return [x, y]
+    return False
+
+
+def get_coord_char(map_in, x, y):
+    # Checks what character is in the specified position
+    return map_in.map_array[::-1][y][x]
+
+
+# Coordinate init code:
+def init_coord():
+    init_coordinates = get_coord(game_data.MapData.current_map)
+    game_data.MapData.y = init_coordinates[1]
+    game_data.MapData.x = init_coordinates[0]
+
+
+def coord_set(map_in, x_m, y_m):  # Main Movement Engine
+    # Move point to a certain position on the map
+    # Collision Checking
+
+    # x_m = Amount to move on x plane
+    # y_m = Amount to move on y plane
+
+    # Check to see if next coordinate pairs is an illegal movement if it is, don't move
+    # if it is not, get the current coordinates and reset the position to a blank spot
+    # move the players char to the new set of coordinates
+    new_x = game_data.MapData.x + x_m
+    new_y = game_data.MapData.y + y_m
+    if not (new_x > len(map_in.map_array[0]) - 1 or new_x < 0 or new_y > len(map_in.map_array) - 1 or new_y < 0):
+        future_char = get_coord_char(map_in, new_x, new_y)
+        if not game_data.Data.movement_blacklist.__contains__(future_char):
+            internal_coordinates = []
+            for yi, y in enumerate(map_in.map_array):
+                for xi, x in enumerate(y):
+                    if x == "x":
+                        internal_coordinates = [xi, yi]
+
+            os.system("cls")
+            game_data.MapData.x += x_m
+            game_data.MapData.y += y_m
+            map_in.map_array[internal_coordinates[1]][internal_coordinates[0]] = ""  # Clear old position
+            map_in.map_array[::-1][game_data.MapData.y][game_data.MapData.x] = "x"  # Enter new position
+            show_map(map_in)
+
+
+def show_map(map_in):
+    # Print the map
+    print("{:^40}".format(map_in.map_name))
+    print(f"{Fore.RED}{'/':^{game_data.Data.map_spacing}}{Fore.RESET}" * len(map_in.map_array[0]))
+    for y in map_in.map_array:
+        cur_row = ""  # Reset the line print out
+        for x in y:
+            # Get Current Character and add it to the formatted line
+            cur_char = x
+            if cur_char == "":
+                cur_char = f"{0:<{game_data.Data.map_spacing}}"
+            elif cur_char == "X":
+                cur_char = f"{Fore.YELLOW}{cur_char:<{game_data.Data.map_spacing}}{Fore.RESET}"
+            elif cur_char == "x":
+                cur_char = f"{Fore.CYAN}{cur_char:<{game_data.Data.map_spacing}}{Fore.RESET}"
+            elif cur_char == "-":
+                cur_char = f"{Fore.LIGHTGREEN_EX}{cur_char:<{game_data.Data.map_spacing}}{Fore.RESET}"
+
+            cur_row += cur_char
+        print(cur_row)
+    print(f"{Fore.RED}{'/':^{game_data.Data.map_spacing}}{Fore.RESET}" * len(map_in.map_array[0]))
+    print(f"Your current position is {get_coord(map_in)} "
+          f"(Represented by the {Fore.GREEN + 'x' + Style.RESET_ALL})")
+    gc.collect()
+
+
+def on_release(key):
+    if key == keyboard.Key.up:
+        Thread(target=coord_set, args=(game_data.MapData.current_map, 0, 1)).start()
+    elif key == keyboard.Key.down:
+        Thread(target=coord_set, args=(game_data.MapData.current_map, 0, -1)).start()
+    elif key == keyboard.Key.right:
+        Thread(target=coord_set, args=(game_data.MapData.current_map, 1, 0)).start()
+    elif key == keyboard.Key.left:
+        Thread(target=coord_set, args=(game_data.MapData.current_map, -1, 0)).start()
+    elif key == keyboard.Key.esc:
+        return False  # Test Code, allows code exit mid run
+
+
+def kb_listener():
+    def listener_start():
+        with keyboard.Listener(on_release=on_release) as listener:
+            def watcher():
+                while True:
+                    if game_data.MapData.map_kill is True:
+                        game_data.MapData.map_kill = False
+                        listener.stop()
+                        return False  # Kill Watcher Thread
+                    time.sleep(0.0001)  # Probably the most important statement in the entire program
+            Thread(target=watcher).start()  # MULTI-THREADING!!!
+            listener.join()
+            print("Listener Ended")
+    Thread(target=listener_start).start()
