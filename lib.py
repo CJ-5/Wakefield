5490cf90a32040f769836799fe7b042c3e6ec796
cj-5 <2006carter.man@gmail.com>
2021-11-30 15:36:22 -0500

    fullscreen script import

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
 #  Holds the main functions that operate the backend of the game (e.g battle system)
-import lib
+import os
+from os import system
 import movement_engine
 import time
 from colorama import init
@@ -9,6 +10,11 @@ import game_data
 import sys
 from ctypes import windll
 import win32gui
+import win32gui, win32com.client
+import ctypes
+import msvcrt
+import subprocess
+from ctypes import wintypes
 
 
 class Logo:
@@ -117,6 +123,37 @@ def is_full_screen():
         return rect == full_screen_rect
     except:
         return False
+
+
+def maximize_console(lines=None):
+    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+    user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+    SW_MAXIMIZE = 3
+    kernel32.GetConsoleWindow.restype = wintypes.HWND
+    kernel32.GetLargestConsoleWindowSize.restype = wintypes._COORD
+    kernel32.GetLargestConsoleWindowSize.argtypes = (wintypes.HANDLE,)
+    user32.ShowWindow.argtypes = (wintypes.HWND, ctypes.c_int)
+
+    fd = os.open('CONOUT$', os.O_RDWR)
+    try:
+        hcon = msvcrt.get_osfhandle(fd)
+        max_size = kernel32.GetLargestConsoleWindowSize(hcon)
+        if max_size.X == 0 and max_size.Y == 0:
+            raise ctypes.WinError(ctypes.get_last_error())
+
+        cols = max_size.X
+        hwnd = kernel32.GetConsoleWindow()
+        if cols and hwnd:
+            if lines is None:
+                lines = max_size.Y
+            else:
+                lines = max(min(lines, 9999), max_size.Y)
+            subprocess.check_call('mode.com con cols={} lines={}'.format(
+                                    cols, lines))
+            user32.ShowWindow(hwnd, SW_MAXIMIZE)
+    finally:
+        os.close(fd)
 
 
 def clear_line(num: int, max_line_length: int, reset: bool, multiple: bool = True, direction: str = 'A'):
